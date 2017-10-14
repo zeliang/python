b257656e59cceb617d6a9a992fc2e801df38decf
liaozeliang <956215293@qq.com>
2017-10-14 17:41:32 +0800

    test

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python  
+# -*- coding: utf-8 -*-
+
 import numpy as np
 
 a=np.array([[1,2,3],[32,1,3],[33,54,65]])
@@ -5,3 +8,9 @@ a=np.array([[1,2,3],[32,1,3],[33,54,65]])
 print a
 
 print a[0,0]
+
+array=np.arange(0,9)
+print array
+
+#支持中文
+print array[3:7]
